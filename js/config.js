5a79096e14730d39bc10a038868e948bc03f17c4
camilomurielph <149724435+camilomurielph@users.noreply.github.com>
2026-07-01 12:07:12 -0500

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// ================================================================
+//  CONFIGURACIÓN DE SUPABASE
+// ================================================================
+export const SUPABASE_URL = 'https://vqfnfggupczdchadoqrj.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxZm5mZ2d1cGN6ZGNoYWRvcXJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5MTI4NzcsImV4cCI6MjA5ODQ4ODg3N30.eD4tJKpNQDz0pOkSD6tIXGcZuKTa2M-fWqSV-QYHWHM';
