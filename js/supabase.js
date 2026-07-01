8313bc8adb1fa89674475d61d7d2cc33a07c8a76
camilomurielph <149724435+camilomurielph@users.noreply.github.com>
2026-07-01 14:13:38 -0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,5 @@
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from './config.js';
 
-// Crear el cliente de Supabase y exportarlo
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export { supabase };
